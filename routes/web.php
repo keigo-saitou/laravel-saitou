9d233a887a6e8184bc5a5f85d8abac47bc9278aa
keigo-saitou <5523041s@st.toho-u.ac.jp>
2025-06-12 05:53:16 +0000

    7-2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-use App\Http\Controllers\PostController;
-
-Route::get('/posts', [PostController::class, 'index']); 
+Route::get('/', [PostController::class, 'index']);
